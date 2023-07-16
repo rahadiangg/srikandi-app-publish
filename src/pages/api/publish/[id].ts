230fd77a3e9cd95ccfd910d61b4f9e665a6e0db3
muttaqinrizal <rizalm57@gmail.com>
2023-07-16 22:00:02 +0700

    feat : handle cors

--- src/pages/api/publish/[id].ts
+++ src/pages/api/publish/[id].ts
@@ -1,11 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
+import NextCors from 'nextjs-cors';
 
 // PUT /api/publish/:id
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+
+  //handle CORS
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+
   const postId = req.query.id
   const post = await prisma.post.update({
     where: { id: Number(postId) },
